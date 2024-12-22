0fa60b313828e6719aa03af628ebdf264b5bcd5f
oskar <oskar.bahr.lg@gmail.com>
2024-12-22 22:22:35 +0100

    [#47] Remove functions object from vercel.ts and move api directory into src directory

--- src/api/vercel.ts
+++ src/api/vercel.ts
@@ -1,5 +1,3 @@
-// Because in vercel the api directory and the content of the dist directory are in the same parent directory
-// @ts-ignore
 import {GET as IMPORTED_GET, POST as IMPORTED_POST} from '../index.js'
 
 export const GET = IMPORTED_GET
