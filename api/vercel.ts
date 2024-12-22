63656bd3f364655523820a7b20437de5d7913216
oskar <oskar.bahr.lg@gmail.com>
2024-12-22 21:53:20 +0100

    [#47] Fixing import in vercel.ts

--- api/vercel.ts
+++ api/vercel.ts
@@ -1,4 +1,6 @@
-import {GET as IMPORTED_GET, POST as IMPORTED_POST} from '../src/index.js'
+// Because in vercel the api directory and the content of the dist directory are in the same parent directory
+// @ts-ignore
+import {GET as IMPORTED_GET, POST as IMPORTED_POST} from '../index.js'
 
 export const GET = IMPORTED_GET
 export const POST = IMPORTED_POST
