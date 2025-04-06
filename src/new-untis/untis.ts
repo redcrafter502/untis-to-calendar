b8c53e9178b14db553b382399adb2f37b5c80218
oskar <76687828+redcrafter502@users.noreply.github.com>
2025-04-06 10:37:23 +0200

    Have working method for getting schhoolyear from untis api and handeling errors with neverthrow

--- src/new-untis/untis.ts
+++ src/new-untis/untis.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { type } from 'arktype'
+import { ok, err, Result } from 'neverthrow'
 
 type GetUntisProps = {
   url: string
@@ -30,6 +31,28 @@ const SessionInformationType = type({
   'jwt_token?': 'string',
 })
 
+const SchoolYearType = type({
+  name: 'string',
+  id: 'number',
+  startDate: 'number',
+  endDate: 'number',
+})
+
+const ClassType = type(
+  {
+    id: 'number',
+    name: 'string',
+    longName: 'string',
+    active: 'boolean',
+    'foreColor?': 'string',
+    'backColor?': 'string',
+    'did?': 'number',
+    'teacher1?': 'number',
+    'teacher2?': 'number',
+  },
+  '[]',
+)
+
 export function getUntis({ url, school, timezone, auth }: GetUntisProps) {
   const identity = 'Awesome'
 
@@ -55,8 +78,48 @@ export function getUntis({ url, school, timezone, auth }: GetUntisProps) {
     },
   })
 
+  const schoolBase64 = '_' + btoa(school)
+
+  async function request<Params>(
+    method: string,
+    params: Params,
+    validateSession = true,
+    session: typeof SessionInformationType.infer,
+    url = '/WebUntis/jsonrpc.do',
+  ): Promise<Result<unknown, string>> {
+    /*if (validateSession && !(await this.validateSession()))
+      return { error: 'Current Session is not valid.' }*/
+    const response = await myAxios({
+      method: 'post',
+      url,
+      params: {
+        school: school,
+      },
+      headers: {
+        Cookie: [
+          `JSESSIONID=${session.sessionId}`,
+          `schoolname?${schoolBase64}`,
+        ].join('; '),
+      },
+      data: {
+        id: identity,
+        method,
+        params,
+        jsonrpc: '2.0',
+      },
+    })
+    if (typeof response.data !== 'object')
+      return err('Failed to parse server response.')
+    if (!response.data.result) return err('Server returned no result.')
+    if (response.data.result.code)
+      return err('Login returned error code: ' + response.data.result.code)
+    return ok(response.data.result)
+  }
+
   return {
-    async login() {
+    async login(): Promise<
+      Result<typeof SessionInformationType.infer, string>
+    > {
       switch (auth.type) {
         case 'password':
           const response = await myAxios({
@@ -77,19 +140,19 @@ export function getUntis({ url, school, timezone, auth }: GetUntisProps) {
             },
           })
           if (typeof response.data !== 'object')
-            return { error: 'Failed to parse server response.' }
+            return err('Failed to parse server response.')
           if (response.data.result.code)
-            return {
-              error: 'Login returned error code: ' + response.data.result.code,
-            }
+            return err(
+              'Login returned error code: ' + response.data.result.code,
+            )
           const validatedResult = SessionInformationType(response.data.result)
           if (validatedResult instanceof type.errors)
-            return { error: validatedResult.summary }
-          return validatedResult
+            return err(validatedResult.summary)
+          return ok(validatedResult)
         case 'secret':
-          return 'hi'
+          return err('No implementation for secret auth')
         case 'public':
-          return 'hi'
+          return err('No implementation for public auth')
       }
     },
     async logout() {
@@ -107,6 +170,35 @@ export function getUntis({ url, school, timezone, auth }: GetUntisProps) {
         },
       })
       return
+    },
+    async getClassesForCurrentSchoolYear(
+      session: typeof SessionInformationType.infer,
+      validateSession = true,
+    ): Promise<Result<typeof ClassType.infer, string>> {
+      const schoolyearData = await request(
+        'getCurrentSchoolyear',
+        {},
+        validateSession,
+        session,
+      )
+      if (schoolyearData.isErr()) return err(schoolyearData.error)
+      const validatedSchoolyearData = SchoolYearType(schoolyearData.value)
+      if (validatedSchoolyearData instanceof type.errors)
+        return err(validatedSchoolyearData.summary)
+
+      const classesData = await request(
+        'getKlassen',
+        {
+          schoolyearId: validatedSchoolyearData.id,
+        },
+        validateSession,
+        session,
+      )
+      if (classesData.isErr()) return err(classesData.error)
+      const validatedClassesData = ClassType(classesData.value)
+      if (validatedClassesData instanceof type.errors)
+        return err(validatedClassesData.summary)
+      return ok(validatedClassesData)
     },
   }
 }
