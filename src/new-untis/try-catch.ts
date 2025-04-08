3dde8d77236878216e5946d7741b8b64decb85d9
oskar <76687828+redcrafter502@users.noreply.github.com>
2025-04-08 15:52:33 +0200

    Use const instead of let

--- src/new-untis/try-catch.ts
+++ src/new-untis/try-catch.ts
@@ -1,12 +1,12 @@
-import { Result, err, ok } from 'neverthrow'
+import { Result, err, ok } from "neverthrow";
 
 export async function tryCatch<T, E = Error>(
   promise: Promise<T>,
 ): Promise<Result<T, E>> {
   try {
-    const data = await promise
-    return ok(data)
+    const data = await promise;
+    return ok(data);
   } catch (error) {
-    return err(error as E)
+    return err(error as E);
   }
 }
