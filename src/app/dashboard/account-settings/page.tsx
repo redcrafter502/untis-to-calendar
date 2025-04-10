61278383cbfb027f75959810ea0a9231b2e10762
oskar <76687828+redcrafter502@users.noreply.github.com>
2025-04-10 09:44:18 +0200

    Make account settings component filling the entire page

--- src/app/dashboard/account-settings/page.tsx
+++ src/app/dashboard/account-settings/page.tsx
@@ -4,7 +4,7 @@ import { Suspense } from "react";
 export default function AccountSettingsPage() {
   return (
     <main>
-      <AccountSettings />
+      <AccountSettings fullPage />
     </main>
   );
 }
