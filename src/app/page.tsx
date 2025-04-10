a0cd76d86ac5dedc20d69bbea50ae98aa6ff82b1
oskar <76687828+redcrafter502@users.noreply.github.com>
2025-04-10 09:11:06 +0200

    Add Get Started button on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import { Button } from "@/components/ui/button";
+import { stackServerApp } from "@/stack";
+import { redirect } from "next/navigation";
 
 export default function Page() {
   return (
     <div className="w-full p-20 text-center">
       <h1>Hello World</h1>
-      <Button>Get Started</Button>
+      <Button
+        onClick={async () => {
+          "use server";
+          await stackServerApp.getUser({ or: "redirect" });
+          redirect("/dashboard");
+        }}
+      >
+        Get Started
+      </Button>
     </div>
   );
 }
