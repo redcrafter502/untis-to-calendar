cb126440d2ec097bef1d851904375acbbcd1af67
oskar <76687828+redcrafter502@users.noreply.github.com>
2025-04-10 14:20:52 +0200

    Filter for state

--- src/app/dashboard/new/page.tsx
+++ src/app/dashboard/new/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { useState } from "react";
 
@@ -7,9 +9,19 @@ export default function NewAccessPage() {
 
   const [pageState, setPageState] = useState(searchParams.get("state"));
 
+  if (pageState === "qrcode") {
+    return <main>QR Code</main>;
+  }
+  if (pageState && ["public", "password", "secret"].includes(pageState)) {
+    return <main>hi</main>;
+  }
+
   return (
-    <main>
-      New {pageState} <button onClick={() => setPageState("page")}>hi</button>
+    <main className="flex min-h-screen w-full flex-col items-center justify-center gap-4">
+      Unknown state: {pageState}
+      <Button asChild>
+        <Link href="/dashboard">Go to Dashboard</Link>
+      </Button>
     </main>
   );
 }
