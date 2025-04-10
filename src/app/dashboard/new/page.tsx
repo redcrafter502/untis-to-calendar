9b0a7f940be22b50888cfd062d783c3ca1f067c1
oskar <76687828+redcrafter502@users.noreply.github.com>
2025-04-10 19:00:05 +0200

    Add create form for password and private accesses

--- src/app/dashboard/new/page.tsx
+++ src/app/dashboard/new/page.tsx
@@ -1,8 +1,33 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { useState } from "react";
+import { type } from "arktype";
+import { useForm } from "react-hook-form";
+import { arktypeResolver } from "@hookform/resolvers/arktype";
+import { Input } from "@/components/ui/input";
+
+const formSchema = type({
+  name: "string",
+  timezone: "string",
+  domain: "string",
+  school: "string",
+  authType: "'public' | 'password' | 'secret'",
+  "classId?": "string",
+  "username?": "string",
+  "password?": "string",
+  "secret?": "string",
+});
 
 export default function NewAccessPage() {
   const searchParams = useSearchParams();
@@ -12,8 +37,22 @@ export default function NewAccessPage() {
   if (pageState === "qrcode") {
     return <main>QR Code</main>;
   }
-  if (pageState && ["public", "password", "secret"].includes(pageState)) {
-    return <main>hi</main>;
+  if (
+    pageState === "public" ||
+    pageState === "password" ||
+    pageState === "secret"
+  ) {
+    return (
+      <main className="mt-4 flex w-full flex-col items-center gap-4">
+        <h1 className="text-4xl">Creating a new {pageState} Access</h1>
+        <CreateForm
+          defaultTimezone="Europe/Berlin"
+          defaultDomain="neilo.webuntis.com"
+          defaultSchool=""
+          authType={pageState}
+        />
+      </main>
+    );
   }
 
   return (
@@ -23,5 +62,185 @@ export default function NewAccessPage() {
         <Link href="/dashboard">Go to Dashboard</Link>
       </Button>
     </main>
+  );
+}
+
+function CreateForm({
+  defaultTimezone,
+  defaultDomain,
+  defaultSchool,
+  authType,
+  defaultUsername,
+  defaultSecret,
+}: {
+  defaultTimezone: string;
+  defaultDomain: string;
+  defaultSchool: string;
+  authType: "public" | "password" | "secret";
+  defaultUsername?: string;
+  defaultSecret?: string;
+}) {
+  const form = useForm<typeof formSchema.infer>({
+    resolver: arktypeResolver(formSchema),
+    defaultValues: {
+      timezone: defaultTimezone,
+      domain: defaultDomain,
+      school: defaultSchool,
+      authType,
+      ...(defaultUsername
+        ? {
+            username: defaultUsername,
+          }
+        : {}),
+      ...(defaultSecret
+        ? {
+            secret: defaultSecret,
+          }
+        : {}),
+    },
+  });
+
+  function onSubmit(values: typeof formSchema.infer) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="My Timetable" {...field} />
+              </FormControl>
+              <FormDescription>
+                The name of your timetable. This will be displayed in the
+                dashboard and your calendar.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="timezone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Timezone</FormLabel>
+              <FormControl>
+                <Input placeholder="Europe/Berlin" {...field} />
+              </FormControl>
+              <FormDescription>
+                The timezone of your timetable. Use the timezone that you see
+                your untis timetable in.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="domain"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Domain</FormLabel>
+              <FormControl>
+                <Input placeholder="neilo.webuntis.com" {...field} />
+              </FormControl>
+              <FormDescription>
+                The domain of your untis. Just open untis and look in the url
+                bar of your browser.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="school"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>School</FormLabel>
+              <FormControl>
+                <Input placeholder="Untis School" {...field} />
+              </FormControl>
+              <FormDescription>
+                The school of your untis. Just open untis and look in the url
+                bar of your browser.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="authType"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Auth Type</FormLabel>
+              <FormControl>
+                <Input disabled {...field} />
+              </FormControl>
+              <FormDescription>
+                The way the timetable should be accessed.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {(authType === "password" || authType === "secret") && (
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="user-456789" {...field} />
+                </FormControl>
+                <FormDescription>Your untis username.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        )}
+        {authType === "password" && (
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="******" {...field} />
+                </FormControl>
+                <FormDescription>Your untis password.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        )}
+        {authType === "secret" && (
+          <FormField
+            control={form.control}
+            name="secret"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Secret</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="******" {...field} />
+                </FormControl>
+                <FormDescription>Your untis secret.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        )}
+        <Button type="submit">Create</Button>
+      </form>
+    </Form>
   );
 }
