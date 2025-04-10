e3f19ef249c16facc61f5ee76245465269aecf8b
oskar <76687828+redcrafter502@users.noreply.github.com>
2025-04-10 23:45:11 +0200

    Improve loading state

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,11 @@
+import { Loader2 } from "lucide-react";
+
 export default function Loading() {
   // Stack uses React Suspense, which will render this page while user data is being fetched.
   // See: https://nextjs.org/docs/app/api-reference/file-conventions/loading
-  return <div>Loading...</div>;
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Loader2 className="size-12 animate-spin" />
+    </div>
+  );
 }
