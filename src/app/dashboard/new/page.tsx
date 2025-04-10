5080088c1deceb925b8755430e37f26343d75c53
oskar <76687828+redcrafter502@users.noreply.github.com>
2025-04-10 14:17:32 +0200

    Add page state and search params for new access page

--- src/app/dashboard/new/page.tsx
+++ src/app/dashboard/new/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
+
 export default function NewAccessPage() {
-  return <main>New</main>;
+  const searchParams = useSearchParams();
+
+  const [pageState, setPageState] = useState(searchParams.get("state"));
+
+  return (
+    <main>
+      New {pageState} <button onClick={() => setPageState("page")}>hi</button>
+    </main>
+  );
 }
