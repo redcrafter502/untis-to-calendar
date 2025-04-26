28205aa7825c8b79d80e21686ea97384f290eedd
oskar <76687828+redcrafter502@users.noreply.github.com>
2025-04-26 23:18:13 +0200

    Testing qr-scanner package

--- src/app/dashboard/new/qr-reader.tsx
+++ src/app/dashboard/new/qr-reader.tsx
@@ -1,8 +1,46 @@
-import { useRef } from "react";
+import QrScanner from "qr-scanner";
+import { useEffect, useRef, useState } from "react";
 
 export function QrReader() {
   const videoRef = useRef<HTMLVideoElement>(null);
   const qrBoxRef = useRef<HTMLDivElement>(null);
+  const scanner = useRef<QrScanner | null>(null);
+  const [qrOn, setQrOn] = useState(false);
+  const [result, setResult] = useState("");
+
+  function onScanSuccess(qrCode: string) {
+    console.log("RESULT: ", qrCode);
+    setResult(qrCode);
+  }
+
+  function onScanFail(err: string | Error) {
+    console.error("SCAN FAILED: ", err);
+  }
+
+  useEffect(() => {
+    if (videoRef.current && !scanner.current) {
+      scanner.current = new QrScanner(videoRef.current, onScanSuccess, {
+        onDecodeError: onScanFail,
+        preferredCamera: "envirement",
+        highlightScanRegion: true,
+        highlightCodeOutline: true,
+        overlay: qrBoxRef.current || undefined,
+      });
+
+      scanner.current
+        .start()
+        .then(() => setQrOn(true))
+        .catch((err) => {
+          if (err) setQrOn(false);
+        });
+    }
+
+    return () => {
+      if (!videoRef.current) {
+        scanner.current?.stop();
+      }
+    };
+  }, []);
 
   return (
     <div className="mx-auto my-0 h-screen w-full max-w-[430px]">
@@ -16,6 +54,19 @@ export function QrReader() {
           height={265}
         />
       </div>
+      {result && (
+        <p
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            zIndex: 99999,
+            color: "white",
+          }}
+        >
+          Scanned Result: {result}
+        </p>
+      )}
     </div>
   );
 }
