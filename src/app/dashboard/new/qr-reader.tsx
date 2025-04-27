e4f8cb52c05449af9958acda671fe107798dd673
oskar <76687828+redcrafter502@users.noreply.github.com>
2025-04-27 09:34:00 +0200

    [#60] Add working qr code scanner

--- src/app/dashboard/new/qr-reader.tsx
+++ src/app/dashboard/new/qr-reader.tsx
@@ -1,3 +1,11 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import QrScanner from "qr-scanner";
 import { useEffect, useRef, useState } from "react";
 
@@ -8,24 +16,27 @@ export function QrReader() {
   const [qrOn, setQrOn] = useState(false);
   const [result, setResult] = useState("");
 
-  function onScanSuccess(qrCode: string) {
-    console.log("RESULT: ", qrCode);
-    setResult(qrCode);
+  function onScanSuccess(qrCode: QrScanner.ScanResult) {
+    setResult(qrCode.data);
   }
 
   function onScanFail(err: string | Error) {
-    console.error("SCAN FAILED: ", err);
+    console.log("SCAN FAILED: ", err);
   }
 
   useEffect(() => {
     if (videoRef.current && !scanner.current) {
-      scanner.current = new QrScanner(videoRef.current, onScanSuccess, {
-        onDecodeError: onScanFail,
-        preferredCamera: "envirement",
-        highlightScanRegion: true,
-        highlightCodeOutline: true,
-        overlay: qrBoxRef.current || undefined,
-      });
+      scanner.current = new QrScanner(
+        videoRef.current,
+        onScanSuccess as unknown as (result: string) => void,
+        {
+          onDecodeError: onScanFail,
+          preferredCamera: "envirement",
+          highlightScanRegion: true,
+          highlightCodeOutline: true,
+          overlay: qrBoxRef.current,
+        } as unknown as number,
+      );
 
       scanner.current
         .start()
@@ -43,30 +54,33 @@ export function QrReader() {
   }, []);
 
   return (
-    <div className="mx-auto my-0 h-screen w-full max-w-[430px]">
-      <video className="h-fit w-full object-cover" ref={videoRef}></video>
-      <div className="left-0 w-full" ref={qrBoxRef}>
-        <img
-          className="absolute top-1/2 left-1/2 fill-none"
-          src="/qr-frame.svg"
-          alt="Qr Frame"
-          width={265}
-          height={265}
-        />
-      </div>
-      {result && (
-        <p
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            zIndex: 99999,
-            color: "white",
-          }}
-        >
-          Scanned Result: {result}
-        </p>
-      )}
+    <div className="flex flex-col items-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>Easily add your Untis Login</CardTitle>
+          <CardDescription>
+            Scan your QR Code from your Untis Account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="w-full max-w-[430px]">
+            <video ref={videoRef}></video>
+            <div className="left-0 w-full" ref={qrBoxRef}>
+              <img
+                className="absolute top-1/2 left-1/2 fill-none"
+                style={{
+                  transform: "translate(-50%, -50%)",
+                }}
+                src="/qr-frame.svg"
+                alt="Qr Frame"
+                width={265}
+                height={265}
+              />
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter>{result && <p>Scanned Result: {result}</p>}</CardFooter>
+      </Card>
     </div>
   );
 }
