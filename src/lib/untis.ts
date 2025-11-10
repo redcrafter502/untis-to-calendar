752f9b550aebb1ee6507d857ca06fbf7b9520a35
oskar <76687828+redcrafter502@users.noreply.github.com>
2025-11-10 20:21:35 +0100

    Console logs for debug

--- src/lib/untis.ts
+++ src/lib/untis.ts
@@ -497,6 +497,8 @@ function convertUntisDateToDate(
     });
 
     const dateStringInTimezone = formatter.format(dateInTimezone);
+    console.log(dateStringInTimezone);
+    console.log(new Date(dateStringInTimezone));
     return ok(new Date(dateStringInTimezone));
   } catch (error) {
     return err(error as Error);
