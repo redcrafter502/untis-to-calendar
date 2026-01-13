56874eb57a337426e98c46561422a67e093ced8f
oskar <76687828+redcrafter502@users.noreply.github.com>
2026-01-13 23:15:32 +0100

    Add Account deletion logic

--- src/app/dashboard/server.ts
+++ src/app/dashboard/server.ts
@@ -1,9 +1,14 @@
 "use server";
 
+import { MUTATIONS } from "@/db/queries";
 import { stackServerApp } from "@/stack";
 
-export async function deleteAccountServerFunction() {
+export async function deleteAccountAction() {
   const user = await stackServerApp.getUser({ or: "redirect" });
   const accesses = (user.serverMetadata?.accesses ?? []) as string[];
   console.log(accesses);
+  accesses.map((access) => {
+    MUTATIONS.deleteAccess(access);
+  });
+  user.delete();
 }
